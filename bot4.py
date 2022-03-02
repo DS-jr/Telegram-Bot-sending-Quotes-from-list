81458a5bdd5973cfdf5787c1ea77ddf11d1790b8
DS-jr <93123740+DS-jr@users.noreply.github.com>
2022-03-02 14:49:10 +0300

    Update bot4.py

--- bot4.py
+++ bot4.py
@@ -5,7 +5,7 @@ my_quotes = [
     "(Ray Dalio) Go to your pain rather than avoid it.",
     "(Paul Graham)  Working hard means constantly aiming towards the core important center of the problem / task: usually the most difficult / uncomfortable part of it.", 
     "(Stephen Hawking)  However difficult life may seem, there is always smth you can do and succeed at.", 
-    "(Lee Kuan Yew, LKY)  Determination creates a huge internal drive & focus. People feel if you are determined or not.", 
+    "(Lee Kuan Yew, LKY)  Determination creates a huge internal drive and focus. People feel if you are determined or not.", 
     "(Goethe)  Treat people as if they were what they ought to be and you help them to become what they are capable of being.", 
     "(Ray Dalio)  Do NOT worry about looking good - worry instead about achieving your goals.", 
     "(Jay Adelson)  A simple test about integrity: if you make a decision, would you be comfortable if everyone knows you made it? Can you make this decision w/ full transparency & feel you’ll NOT have to hide anything?"
